e78c80049251084609ebe3862832e2661e95c52b
André Azevedo <aazevedo525@gmail.com>
2025-05-15 18:12:18 -0300

    alteração no caminho de routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import routes from "./src/routes/routes.js";
+import routes from "./routes/routes.js";
 
 dotenv.config();
 
