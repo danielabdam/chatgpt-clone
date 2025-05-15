f3f62ea8f99c03d5068bc8ffbc8bae042218c36b
André Azevedo <aazevedo525@gmail.com>
2025-05-15 17:58:35 -0300

    Arquivo de configuração Jest

---
+++ server/jest.config.mjs
@@ -0,0 +1,9 @@
+// Arquivo de configuração vazio para Jest em projetos ESM. Nenhuma configuração extra é necessária.
+export default {
+  verbose: true,
+  testEnvironment: "node",
+  testMatch: [
+    "<rootDir>/testes/**/*.js",
+    "<rootDir>/testes/**/*.Teste.js"
+  ]
+};
