76849fe50fc51d6742204a1b4a46a5eee160af9c
Daniel Abdam <daniellucas227@gmail.com>
2025-05-17 19:16:13 -0300

    Ajustado responsividade do chat
    Ajuste do ChatMessages.js para resposta em tela

--- web/src/components/chat/ChatMessages.js
+++ web/src/components/chat/ChatMessages.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const ChatMessages = ({ messages, messagesEndRef }) => {
+  // Força re-renderização quando messages mudar
+  React.useEffect(() => {}, [messages]);
+
   return (
-    <div className="chat-box">
-      <div className="messages">
-        {messages.map((msg, index) => (
-          <div key={index} className={`message ${msg.type}`}>
-            {msg.text}
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </div>
+    <div className="messages">
+      {messages.map((msg, index) => (
+        <div key={index} className={`message ${msg.type}`}>
+          {msg.text}
+        </div>
+      ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
